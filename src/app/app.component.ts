ba01d643cf74f7b06e643359a02a08f463772faa
1db8k <1db8k@live.com>
2017-05-11 10:43:25 +0200

    Add Example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers } from "@angular/http";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  obj = { hi: 'hi', inside: 'example' }
+  private headers = new Headers({'Content-Type': 'application/json'});
+
+  constructor(public http: Http) {}
+
+  onClick() {
+    // this.http.post('/', JSON.stringify(this.obj), {headers : this.headers})
+    this.http.post('/', this.obj, {headers : this.headers})
+      .subscribe((response) => {console.log(response)})
+  }
+
 }
---
+++ src/server/server.js
@@ -0,0 +1,31 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+
+// var bodyParser = require('body-parser')
+
+// // parse application/x-www-form-urlencoded
+// app.use(bodyParser.urlencoded({ extended: false }))
+
+// // parse application/json
+// app.use(bodyParser.json())
+
+app.use(express.static(path.join(__dirname, '../../dist')))
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, '../../dist/index.html'))
+})
+
+app.post('/', function (req, res) {
+  let data = ''
+  req.on('data', function (chunk) {
+    data += chunk
+  })
+  req.on('end', function () {
+    data // will be the same here regardless of whether I use bodyParser here in the back end and/or JSON.stringify on the front end
+  })
+})
+
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!')
+})
